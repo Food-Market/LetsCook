0b0fd74898868897d46ad2b33f60e60a20e4bfa6
garciaarmando <armandobfmv@gmail.com>
2020-08-27 22:31:51 -0500

    Converting to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import React from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Layout from "./components/Layout"
-import Recipes from "./pages/Recipes"
-import Login from "./pages/Login"
-import SignUp from "./pages/SignUp"
-import PageError from "./pages/PageError"
+import Layout from "./components/Layout";
+import Recipes from "./pages/Recipes";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import PageError from "./pages/PageError";
+import RecipeDetails from "./pages/recipeDetails";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <Switch>
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/signup" component={SignUp} />
-                    <Route exact path="/recipes" component={Recipes} />
-                    <Route component={PageError} />
-                </Switch>
-            </Layout>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/recipes" component={Recipes} />
+          <Route exact path="/recipeDetails" component={RecipeDetails} />
+          <Route component={PageError} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
