0f37bb2633a59901691311a4f0129330482514d5
andrés ricaño cesar <aricanomx@gmail.com>
2020-08-31 20:22:13 -0500

    internal adds

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react'; //para usar JSX (HTMl dentro de JS)
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
-// import App from "./App"
 import Payments from './pages/Payments';
-
 ReactDOM.render(<Payments />, document.getElementById('app'));
+
+// import App from './App';
+// ReactDOM.render(<App />, document.getElementById('app'));
