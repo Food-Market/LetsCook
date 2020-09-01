785807dff536785be399e0b1f37881c5356afcef
andrés ricaño cesar <aricanomx@gmail.com>
2020-08-31 21:16:36 -0500

    Config class main

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,12 @@
-import React from "react"; //para usar JSX (HTMl dentro de JS)
-import ReactDOM from "react-dom";
-import "./styles/styles.scss";
+import React from 'react'; //para usar JSX (HTMl dentro de JS)
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
 
-<<<<<<< HEAD
-import Payments from './pages/Payments';
-ReactDOM.render(<Payments />, document.getElementById('app'));
+// import Payments from './pages/Payments';
+// ReactDOM.render(<Payments />, document.getElementById('app'));
 
 // import App from './App';
-// ReactDOM.render(<App />, document.getElementById('app'));
-=======
-// import App from "./App"
-import Payments from "./pages/Payments";
-
-ReactDOM.render(<Payments />, document.getElementById("app"));
->>>>>>> origin/development
+// import Profile from './pages/Profile';
+// ReactDOM.render(<Profile />, document.getElementById('app'));
+import RecipeDetails from './pages/recipeDetails';
+ReactDOM.render(<RecipeDetails />, document.getElementById('app'));
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,40 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
   return (
     <React.Fragment>
-      <section className="information__container">
-        <section className="information__AvatarNameEmail">
-          <img src="https://i.imgur.com/h2W3BCh.png" alt="Profile Image" />
-          <Link to="/" className="information__Name">
-            {" "}
-            Armando García{" "}
-          </Link>
-          <Link to="/" className="information__email">
-            {" "}
-            armandobfmv@gmail.com{" "}
-          </Link>
+      <section className='information__container'>
+        <section className='information__AvatarNameEmail'>
+          <img src='https://i.imgur.com/h2W3BCh.png' alt='Profile Image' />
+          <a href='/' className='information__Name'>
+            {' '}
+            Armando García{' '}
+          </a>
+          <a href='/' className='information__email'>
+            {' '}
+            armandobfmv@gmail.com{' '}
+          </a>
         </section>
 
-        <section className="information__addresses">
+        <section className='information__addresses'>
           <h2>My Addresses</h2>
           <p>P Sherman, Street Wallaby 42, Sydney</p>
         </section>
 
-        <section className="information__payment">
+        <section className='information__payment'>
           <h2>Payment Methods</h2>
-          <div className="information__payment__container">
+          <div className='information__payment__container'>
             <img
-              src="../styles/assets/icons/visa.svg"
-              alt="Payment Method Logo"
+              src='../styles/assets/icons/visa.svg'
+              alt='Payment Method Logo'
             />
-            <div className="information__payment__number">#</div>
-            <div className="information__payment__due">Due Date: 01/2028"</div>
+            <div className='information__payment__number'>#</div>
+            <div className='information__payment__due'>Due Date: 01/2028"</div>
           </div>
         </section>
 
-        <section className="information__suscription">
+        <section className='information__suscription'>
           <h2>Suscription Information</h2>
           <p>Premium subscription activated</p>
           <p>
@@ -42,7 +42,7 @@ const Profile = () => {
           </p>
         </section>
 
-        <section className="information__ordersHistory">
+        <section className='information__ordersHistory'>
           <h2>Orders History</h2>
           <table>
             <tr>
@@ -54,7 +54,7 @@ const Profile = () => {
 
             <tr>
               <td>
-                <Link to="">ID #</Link>
+                <a>ID #</a>
               </td>
               <td>DDMMAAAA</td>
               <td>$</td>
