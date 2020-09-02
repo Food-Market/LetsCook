8ce4f925096adb4dca9a109d7d91ad143e1b5f3f
andrés ricaño cesar <aricanomx@gmail.com>
2020-09-01 20:35:21 -0500

    resolve a problem with router library

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'; //para usar JSX (HTMl dentro de JS)
 import ReactDOM from 'react-dom';
+
 import './styles/styles.scss';
 
 // import Payments from './pages/Payments';
 // ReactDOM.render(<Payments />, document.getElementById('app'));
 
-// import App from './App';
+import App from './App';
 // import Profile from './pages/Profile';
 // ReactDOM.render(<Profile />, document.getElementById('app'));
-import RecipeDetails from './pages/recipeDetails';
-ReactDOM.render(<RecipeDetails />, document.getElementById('app'));
+// import RecipeDetails from './pages/recipeDetails';
+ReactDOM.render(<App />, document.getElementById('app'));
